6b2561233e30b9de6d232cd39f93a64255a35106
Rohit Murthy <rohitmur@andrew.cmu.edu>
2017-11-14 18:46:45 -0500

    More generalized code

--- python_polyfit.py
+++ python_polyfit.py
@@ -3,28 +3,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-frame = np.linspace(1, 20, num = 20)
+observation_length = 12
+prediction_length = 8
 
-x_true =  np.zeros(20)
-y_true = np.zeros(20)
-x_pred = np.zeros(20)
-y_pred = np.zeros(20)
+frame = np.linspace(1, observation_length + prediction_length, num = observation_length + prediction_length)
 
-shape_error = (20, 2)
+x_true =  np.zeros(observation_length + prediction_length)
+y_true = np.zeros(observation_length + prediction_length)
+x_pred = np.zeros(observation_length + prediction_length)
+y_pred = np.zeros(observation_length + prediction_length)
+
+shape_error = (observation_length + prediction_length, 2)
 error = np.zeros(shape_error)
+
+polyfit_degree = 3
 
 x_true = np.cos(frame * 9 * np.pi / 180)
 y_true = np.sin(frame * 9 * np.pi / 180)
 
-p_x = np.polyfit(frame[1:12], x_true[1:12], 4)
-p_y = np.polyfit(frame[1:12], y_true[1:12], 4)
-x_pred[0:12] = x_true[0:12]
-y_pred[0:12] = y_true[0:12]
+p_x = np.polyfit(frame[0:observation_length], x_true[0:observation_length], polyfit_degree)
+p_y = np.polyfit(frame[0:observation_length], y_true[0:observation_length], polyfit_degree)
+x_pred[0:observation_length] = x_true[0:observation_length]
+y_pred[0:observation_length] = y_true[0:observation_length]
 
 p_x = np.poly1d(p_x)
 p_y = np.poly1d(p_y)
 
-for i in range(12,20):
+for i in range(observation_length, observation_length + prediction_length):
 	x_pred[i] = p_x(frame[i])
 	y_pred[i] = p_y(frame[i])
 	error[i][0] = x_pred[i] - x_true[i]
